5d7009a0140399c21ca54dde075805de094590f3
William Ventura <vg18011@ues.edu.sv>
2024-11-06 11:17:08 -0600

    cambios en el home

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,16 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <p>&copy; {{ date('Y') }} CrediRapidoSV. Todos los derechos reservados.</p>
+            </div>
+            <div class="col-md-6 text-md-right">
+                <ul class="list-inline">
+                    <li class="list-inline-item"><a href="#">Política de Privacidad</a></li>
+                    <li class="list-inline-item"><a href="#">Términos de Servicio</a></li>
+                    <li class="list-inline-item"><a href="#">Contacto</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
